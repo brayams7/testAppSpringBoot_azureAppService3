c7d9f2b15711c9f675493aa939e29fec118d22d4
brayams <brayamsbilgs7govy@gmail.com>
2022-08-31 01:30:14 +0200

    proyecto inicial

---
+++ src/main/java/com/springBoot/testAppAzureAppService2/TestAppAzureAppService2Application.java
@@ -0,0 +1,13 @@
+package com.springBoot.testAppAzureAppService2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppAzureAppService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAppAzureAppService2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot/testAppAzureAppService2/TestAppAzureAppService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.testAppAzureAppService2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppAzureAppService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
